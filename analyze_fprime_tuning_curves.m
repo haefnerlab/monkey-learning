94c18f497e01c3fe041c7cf692793586b5a49720
wrongu <rlange@ur.rochester.edu>
2015-08-11 17:02:28 -0400

    analyze_fprime_tuning_curves easily changeable vonMises <--> piecewise linear

--- analyze_fprime_tuning_curves.m
+++ analyze_fprime_tuning_curves.m
@@ -22,6 +22,7 @@ end
 pops_task = Split_Conditions( pops_task );
 pops_task = Compute_fPrime_stimulus_means( pops_task );
 pops_task = Compute_fPrime_bestfit( pops_task, pops_fix );
+pops_task = Compute_fPrime_fixation_means( pops_task, pops_fix );
 
 save(savefile, 'pops_task', 'pops_fix');
 
@@ -29,8 +30,10 @@ n_pops = length(pops_task);
 n_neurons = sum(arrayfun(@(p) length(p.cellnos), pops_task));
 
 verbose = true;
-min_pairs = 25; % min # n-tuples of not-NaN trials for the moment to be considered valid
-min_rates = 10; % min avg # spikes in the n-tuple for a trial to be counted
+fprime_curve = 'tuning_vm_curves'; % f' defined by von Mises best-fit curve
+% fprime_curve = 'tuning_pw_curves'; % f' defined by piecewise linear curve
+min_pairs = 25; % (TODO?) min # n-tuples of not-NaN trials for the moment to be considered valid
+min_rates = 10; % (TODO?) min avg # spikes in the n-tuple for a trial to be counted
 popcolors = hsv(n_pops);
 
 close all;
@@ -57,8 +60,8 @@ for p_idx=1:n_pops
     pop = pops_task(p_idx);
     popsize = length(pop.cellnos);
     choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
-%     fprime_at_task = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.tuning_vm_curves{n_idx}, pop.Orientation), 1:popsize);
-    fprime_at_task = pop.fprime_stimulus_mean;
+    fprime_at_task = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(fprime_curve){n_idx}, pop.Orientation), 1:popsize);
+    
     scatter(fprime_at_task, choice_triggered_delta_means, 5, popcolors(p_idx,:));
     
     end_idx = start_idx + popsize - 1;
@@ -83,7 +86,7 @@ for p_idx=1:n_pops
     pop = pops_task(p_idx);
     popsize = length(pop.cellnos);
     choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
-    fprime_off_task = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.tuning_vm_curves{n_idx}, pop.Orientation + 45), 1:popsize);
+    fprime_off_task = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(fprime_curve){n_idx}, pop.Orientation + 45), 1:popsize);
     scatter(fprime_off_task, choice_triggered_delta_means, 5, popcolors(p_idx,:));
     
     end_idx = start_idx + popsize - 1;
@@ -102,8 +105,8 @@ ylabel('choice-triggered diff means')
 offsets = -90:5:90;
 
 all_correlations = zeros(1,length(offsets));
-for o_idx=1:length(offsets)
-    offset = offsets(o_idx);
+parfor o_idx=1:length(offsets)
+    offset = offsets(o_idx)
     all_fprimes = zeros(n_neurons,1);
     all_ctdms = zeros(n_neurons,1);
     start_idx = 1;
@@ -111,7 +114,7 @@ for o_idx=1:length(offsets)
         pop = pops_task(p_idx);
         popsize = length(pop.cellnos);
         choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
-        fprime_at_offset = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.tuning_vm_curves{n_idx}, pop.Orientation + offset), 1:popsize);
+        fprime_at_offset = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(fprime_curve){n_idx}, pop.Orientation + offset), 1:popsize);
     
         end_idx = start_idx + popsize - 1;
         all_fprimes(start_idx:end_idx) = fprime_at_offset;
