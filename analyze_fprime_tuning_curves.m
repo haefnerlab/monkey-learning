93cc3f4d771c269b68ecdce1f41a46cb250e76cd
wrongu <rlange@ur.rochester.edu>
2015-08-17 17:56:18 -0400

    big refactor of analyze_fprime_tuning_curves: all precomputed at start
    (in preparation for analyzing higher-order moments)

--- analyze_fprime_tuning_curves.m
+++ analyze_fprime_tuning_curves.m
@@ -8,7 +8,6 @@ function analyze_fprime_tuning_curves( params )
 %   'tuning_pw_curves' to use piecewise-linear interpolations from the
 %   fixation task as the tuning curve
 
-
 %% Load and preprocess
 
 savefile = fullfile('data', params.monkey, 'preprocessed.mat');
@@ -40,98 +39,108 @@ n_pops = length(pops_task);
 n_neurons = sum(arrayfun(@(p) length(p.cellnos), pops_task));
 popcolors = hsv(n_pops);
 
+% anonymous function to get f' from a tuning curve at a given stimulus
 TuningCurve_fPrime_At = @(curve, angle) (curve(angle) - curve(angle+90));
 
-%% PLOT I: scatter and correlation when f' aligned with task
-if params.verbose, fprintf('Left plot: f'' task vs CT moment\n'); end
-all_fprimes = zeros(n_neurons,1);
-all_ctdms = zeros(n_neurons,1);
+% prepare scatter plot colors (neurons colored by population)
+neuroncolors = zeros(n_neurons,3);
 start_idx = 1;
+for p_idx=1:n_pops
+    pop = pops_task(p_idx);
+    popsize = length(pop.cellnos); 
+    
+    end_idx = start_idx + popsize - 1;
+    neuroncolors(start_idx:end_idx,:) = repmat(popcolors(p_idx,:),popsize,1);
+    start_idx = end_idx + 1;
+end
 
-figure();
-hold on;
+%% Compute "choice-triggered" moment
+
+if params.verbose, fprintf('computing ctdms..\n'); end
+
+all_ctdms = zeros(n_neurons,1);
+
+% loop over populations to fill in (flattened) all_ctdms array, where now
+% each entry corresponds to a neuron
+start_idx = 1;
 for p_idx=1:n_pops
     pop = pops_task(p_idx);
     popsize = length(pop.cellnos);
-    choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
-    fprime_at_task = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(params.fprime_curve){n_idx}, pop.Orientation), 1:popsize);
     
-    scatter(fprime_at_task, choice_triggered_delta_means, 5, popcolors(p_idx,:));
+    choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
     
     end_idx = start_idx + popsize - 1;
-    all_fprimes(start_idx:end_idx) = fprime_at_task;
     all_ctdms(start_idx:end_idx) = choice_triggered_delta_means;
     start_idx = end_idx + 1;
 end
-hold off;
-[R,P] = corrcoef(all_fprimes, all_ctdms);
-title(sprintf('Corr. choice-triggered means vs task f''\nr=%.3f p=%.3e', R(2), P(2)));
-xlabel('f'' aligned to task')
-ylabel('choice-triggered diff means')
 
-%% PLOT II: scatter and correlation when f' 45 degrees off task
-if params.verbose, fprintf('Middle plot: f'' ortho vs CT moment\n'); end
-all_fprimes = zeros(n_neurons,1);
-all_ctdms = zeros(n_neurons,1);
-start_idx = 1;
+%% Compute f' moment at each of a range of offsets from the task
 
-figure();
-hold on;
-for p_idx=1:n_pops
-    pop = pops_task(p_idx);
-    popsize = length(pop.cellnos);
-    choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
-    fprime_off_task = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(params.fprime_curve){n_idx}, pop.Orientation + 45), 1:popsize);
-    scatter(fprime_off_task, choice_triggered_delta_means, 5, popcolors(p_idx,:));
+if params.verbose, fprintf('computing fprimes..\n'); end
+
+% TODO: make # offsets controllable in params
+offsets = unique(horzcat(-90:5:90, [0,45]));
+
+all_fprimes = zeros(n_neurons, length(offsets)); % each column corresponds to one task-offset
+for o_idx=1:length(offsets)
+    if params.verbose, fprintf('\t%d/%d\n', o_idx, length(offsets)); end
+    offset = offsets(o_idx);
     
-    end_idx = start_idx + popsize - 1;
-    all_fprimes(start_idx:end_idx) = fprime_off_task;
-    all_ctdms(start_idx:end_idx) = choice_triggered_delta_means;
-    start_idx = end_idx + 1;
+    start_idx = 1;
+    for p_idx=1:n_pops
+        pop = pops_task(p_idx);
+        popsize = length(pop.cellnos);
+
+        fprime_at_offset = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(params.fprime_curve){n_idx}, pop.Orientation + offset), 1:popsize);
+    
+        end_idx = start_idx + popsize - 1;
+        all_fprimes(start_idx:end_idx, o_idx) = fprime_at_offset;
+        start_idx = end_idx + 1;
+    end
 end
-hold off;
-[R,P] = corrcoef(all_fprimes, all_ctdms);
-title(sprintf('Corr. choice-triggered means vs off-task f''\nr=%.3f p=%.3e', R(2), P(2)));
-xlabel('f'' aligned 45 degrees off task')
-ylabel('choice-triggered diff means')
 
-%% PLOT III: interpolate (I) and (II): correlation as a function of distance off task
-if params.verbose, fprintf('Right plot: correlation as fn of offset\n'); end
+%% loop to get bootstrapped confidence intervals at each offset
 
-offsets = -90:5:90;
+if params.verbose, fprintf('computing correlations..\n'); end
 
 all_correlations = zeros(1,length(offsets));
 confidence_intervals = zeros(2, length(offsets));
 for o_idx=1:length(offsets)
     if params.verbose, fprintf('\t%d/%d\n', o_idx, length(offsets)); end
-    offset = offsets(o_idx);
-    all_fprimes = zeros(n_neurons,1);
-    all_ctdms = zeros(n_neurons,1);
-    start_idx = 1;
-    for p_idx=1:n_pops
-        pop = pops_task(p_idx);
-        popsize = length(pop.cellnos);
-        choice_triggered_delta_means = (nanmean(pop.spikeCounts_choiceA,2)-nanmean(pop.spikeCounts_choiceB,2))';
-        fprime_at_offset = arrayfun(@(n_idx) TuningCurve_fPrime_At(pop.(params.fprime_curve){n_idx}, pop.Orientation + offset), 1:popsize);
     
-        end_idx = start_idx + popsize - 1;
-        all_fprimes(start_idx:end_idx) = fprime_at_offset;
-        all_ctdms(start_idx:end_idx) = choice_triggered_delta_means;
-        start_idx = end_idx + 1;
-    end
-    % bootstrap confidence intervals, N=1000
-    nboot=1000;
-    
-    % we occasionally get NaN in only some portion of the tuning curve
-    %  when neurons aren't well-behaved; drop those values.
-    valid_indices = ~isnan(all_fprimes) & ~isnan(all_ctdms);
-    all_fprimes = all_fprimes(valid_indices);
-    all_ctdms = all_ctdms(valid_indices);
+    fprimes_this_offset = all_fprimes(:, o_idx);
+    valid_indices = ~isnan(fprimes_this_offset) & ~isnan(all_ctdms);
         
-    all_correlations(o_idx) = corr(all_fprimes, all_ctdms);
-    confidence_intervals(:,o_idx) = bootci(nboot, {@corr, all_fprimes, all_ctdms}, ...
+    all_correlations(o_idx) = corr(fprimes_this_offset(valid_indices), all_ctdms(valid_indices));
+    confidence_intervals(:,o_idx) = bootci(params.bootstrap, ...
+        {@corr, fprimes_this_offset(valid_indices), all_ctdms(valid_indices)}, ...
         'Options', statset('UseParallel', true));
 end
+
+%% PLOT I: scatter and correlation when f' aligned with task
+if params.verbose, fprintf('Left plot: f'' task vs CT moment\n'); end
+
+o_idx = offsets==0;
+
+figure();
+scatter(all_fprimes(:, o_idx), all_ctdms, 15, neuroncolors);
+title(sprintf('Corr. choice-triggered means vs task f''\nr=%.3f +/- %.3e', all_correlations(o_idx), diff(confidence_intervals(:,o_idx))));
+xlabel('f'' aligned to task')
+ylabel('choice-triggered diff means')
+
+%% PLOT II: scatter and correlation when f' 45 degrees off task
+if params.verbose, fprintf('Middle plot: f'' ortho vs CT moment\n'); end
+
+o_idx = offsets==45;
+
+figure();
+scatter(all_fprimes(:, o_idx), all_ctdms, 15, neuroncolors);
+title(sprintf('Corr. choice-triggered means vs off-task f''\nr=%.3f +/- %.3e', all_correlations(o_idx), diff(confidence_intervals(:,o_idx))));
+xlabel('f'' aligned 45 degrees off task')
+ylabel('choice-triggered diff means')
+
+%% PLOT III: interpolate (I) and (II): correlation as a function of distance off task
+if params.verbose, fprintf('Right plot: correlation as fn of offset\n'); end
 
 figure();
 errorbar(offsets, all_correlations, confidence_intervals(1,:), confidence_intervals(2,:));
