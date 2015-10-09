588d177b0ff3edd338b79e6fb85ea3078fc4617e
wrongu <rlange@ur.rochester.edu>
2015-10-09 15:17:38 -0400

    brought TuningCurves.test_vonmises up to speed with formatting practices

--- +TuningCurves/test_vonmises.m
+++ +TuningCurves/test_vonmises.m
@@ -15,15 +15,23 @@ legend('analytic', 'diff-approximation');
 %% TEST FIT
 clearvars; close all;
 
-pops = Load_Fixation_Data('jbe');
-popst = Load_Task_Data('jbe');
+pops_fix = Load_Fixation_Data('jbe');
+pops_task = Load_Task_Data('jbe');
+
+[pops_task, pops_fix] = Match_Corresponding_Populations(pops_task, pops_fix);
 
 f = figure();
 
 os = linspace(0,180,201);
 
-for pi=1:length(pops)
-    pop = pops(pi);
+n_neurons = sum(arrayfun(@(pop) length(pop.cellnos), pops_fix));
+tun = zeros(1,n_neurons);
+fve = zeros(1,n_neurons);
+amp = zeros(1,n_neurons);
+j = 1;
+
+for pi=length(pops_fix):-1:1
+    pop = pops_fix(pi);
     
     % condVecLabel is a cell array of strings identifying contents of each
     %   condVec column. Find which one is 'orientation'.
@@ -31,34 +39,63 @@ for pi=1:length(pops)
     
     orientations = pop.condVec(:,orientation_condition);
     for ni=1:length(pop.cellnos)
+        fprintf('%d/%d\n', j, n_neurons);
         counts = pop.spikeCounts(ni,:);
         
         if numel(counts) == numel(orientations)
-        
+            
             [best, curve, best_map, worst, worst_map] = TuningCurves.fitVonMises(orientations, counts);
             disp(best);
-            disp(best_map);
-            disp(worst);
-            disp(worst_map);
-
+%             disp(best_map);
+%             disp(worst);
+%             disp(worst_map);
+            
             % scatter plot with tuning curve overlayed
-            scatter(orientations, counts);
-            hold on;
-            plot(os, TuningCurves.vonMises(os, best), 'LineWidth', 2);
-            plot(os, TuningCurves.vonMises(os, worst), 'LineStyle', '--');
+            %             scatter(orientations, counts);
+            %             hold on;
+            %             plot(os, TuningCurves.vonMises(os, best), 'LineWidth', 2);
+            %             plot(os, TuningCurves.vonMises(os, worst), 'LineStyle', '--');
             % plot where we thing preferred orientation is (black)
-            plot([best(4) best(4)], [0, max(counts)], '--k')
+            %             plot([best(4) best(4)], [0, max(counts)], '--k')
             % neurons already have some estimated tuning from A.B. et al
             % (green)
-            et = popst(pi).tuning(ni);
-            if ~isnan(et)
-                plot([et et], [0, max(counts)], '--g')
+            %             et = popst(pi).tuning(ni);
+            %             if ~isnan(et)
+            %                 plot([et et], [0, max(counts)], '--g')
+            %             end
+            %             axis([0,180,0,max(counts)+10]);
+            %             hold off;
+            %             title(sprintf('Population %d Neuron %d', pi, ni));
+            
+            amp(j) = best(2) * exp(best(3));
+            fve(j) = Util.Variance_Explained(counts, TuningCurves.vonMises(orientations, best));
+            tun(j) = pops_task(pi).tuning(ni);
+            if isnan(fve(j))
+                keyboard
             end
-            axis([0,180,0,max(counts)+10]);
-            hold off;
-            title(sprintf('Population %d Neuron %d', pi, ni));
-
-            pause; 
+%             if (fve(j) < 0.2 && ~isnan(tun(j))) || isnan(tun(j))
+%                 % look at ones without VE but with tuning well-defined?
+%                 % scatter plot with tuning curve overlayed
+%                 scatter(orientations, counts);
+%                 hold on;
+%                 plot(os, TuningCurves.vonMises(os, best), 'LineWidth', 2);
+%                 plot(os, TuningCurves.vonMises(os, worst), 'LineStyle', '--');
+%                 % plot where we thing preferred orientation is (black)
+%                 plot([best(4) best(4)], [0, max(counts)], '--k')
+%                 % neurons already have some estimated tuning from A.B. et al
+%                 % (green)
+%                 et = pops_task(pi).tuning(ni);
+%                 if ~isnan(et)
+%                     plot([et et], [0, max(counts)], '--g')
+%                 else
+%                     plot([best(4) best(4)], [0, max(counts)], '--r')
+%                 end
+%                 axis([0,180,0,max(counts)+10]);
+%                 hold off;
+%                 title(sprintf('Population %d Neuron %d', pi, ni));
+%                 drawnow; pause;
+%             end
+            j = j+1;
         else
             warning('#counts was not same as #orientations');
         end
