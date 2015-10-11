ebbdd571b7c84f126451fe334c72f33d6e4bcfc0
wrongu <rlange@ur.rochester.edu>
2015-10-11 10:23:22 -0400

    fixed nancomoment (subtract mean *after* filter), and added norm_variance

--- +Util/nancomoment.m
+++ +Util/nancomoment.m
@@ -1,16 +1,18 @@
-function [ output, counts, indices, all_indices ] = nancomoment( X, order, symmetries, minimum_count, minimum_value )
+function [ output, counts, indices, all_indices ] = nancomoment( X, order, symmetries, norm_variance, minimum_count, minimum_value )
 %NANCOMOMENT get the order-th co-moment of X, ignoring NaN values, where each
 % column of X is a variable and each row an observation
 %
 % output is an ndarray with 'order' dimensions, each of size #variables.
 %
-% [output, counts, indices] = nancomoment( X, order, [symmetries, [minimum_count, [minimum_value]]] )
+% [output, counts, indices] = nancomoment( X, order, [symmetries, [norm_variance, [minimum_count, [minimum_value]]]] )
 %
 % output[i,j,k,...] is the expected (i.e. mean) value of
 %   (X(:,i)-u(i))*(X(:,j)-u(j))*(X(:,k)-u(k))*...
 % where 'u' is the mean of each variable, and we discard any (i,j,k,...)
 % pairs where one or more of the measurements is NaN, and the mean of the
-% measurements is above minimum_value
+% measurements is above minimum_value. If norm_variance is true, the output
+% is divided by sqrt(var(i)*var(j)*var(k)*...) (i.e. in moment=2 case, this
+% gives correlation rather than covariance)
 %
 % counts[i,j,k,...] is the number of non-nan observation involving i,j, and
 % k. Where counts is less than minimum_count, output is NaN
@@ -26,8 +28,9 @@ function [ output, counts, indices, all_indices ] = nancomoment( X, order, symme
 if order <= 0, error('order of moments must be greater than 0'); end
 
 if nargin < 3, symmetries=false; end
-if nargin < 4, minimum_count=1; end
-if nargin < 5, minimum_value=-inf; end
+if nargin < 4, norm_variance=false; end
+if nargin < 5, minimum_count=1; end
+if nargin < 6, minimum_value=-inf; end
 
 if order == 1
     output = nanmean(X, 1); % built-in function is faster for means
@@ -39,9 +42,6 @@ else
     all_indices = 1:numel(nddots);
     if symmetries, all_indices = find(Util.ndtriu(size(nddots))); end
     
-    % subtract mean from X
-    Xzero = X - repmat(nanmean(X), size(X,1), 1);
-    
     % ind2sub uses varargout, which we will capture in this cell array
     nd_idxs_cell = cell(1,order);
     
@@ -49,22 +49,35 @@ else
         [nd_idxs_cell{:}] = ind2sub(osize, i);
         idxs = cell2mat(nd_idxs_cell);
         
-        vecs = Xzero(:,idxs);
+        vecs = X(:,idxs);
         
         % find which indices are not NaN for all vecs across observations
         all_valid_observations = all(~isnan(vecs), 2);
         
         % also only keep those where the mean (across vecs) is >
         % minimum_value
-        all_valid_observations = all_valid_observations & mean(X(:,idxs),2) > minimum_value;
+        all_valid_observations = all_valid_observations & mean(vecs,2) > minimum_value;
         
         % count how many data points survived the not-nan and min-value
         % filters
         counts(i) = sum(all_valid_observations);
         
+        % subtract mean from vecs (important: we have to do this *after*
+        % determining which observations are kept so as not to skew the
+        % data)
+        vecs = vecs(all_valid_observations,:);
+        vecs = vecs - ones(counts(i),1) * mean(vecs,1);
+        
         % compute order-th moment
         % like a dot product with <order>-many vectors instead of just 2
-        nddots(i) = sum(prod(vecs(all_valid_observations, :), 2), 1);
+        nddots(i) = sum(prod(vecs, 2), 1);
+        
+        if norm_variance
+            % normalize by sqrt(var i * var j * var k ...), where we use only
+            % the measurements that passed the 'validity' filters to compute
+            % the variance
+            nddots(i) = nddots(i) / sqrt(prod(nanvar(vecs,1)));
+        end
     end
     
     indices = all_indices;
