3af4909191ac794a5d80eb16ec12c762e554f180
wrongu <rlange@ur.rochester.edu>
2015-10-13 11:10:12 -0400

    Convert_Sampling does spikeTimes so that Match_Corresponding_Populations will work

--- +scripts/make_sim_data.m
+++ +scripts/make_sim_data.m
@@ -1,21 +1,29 @@
-% with 'otune' in the workspace..
+% with 'e' and 'otune' in the workspace..
+% where 'e' is result of Sampling Model/S_Run_Experiments
+% and 'otune' is the result of Sampling Model/Orientation_Tuning_Like(e,..)
 
-deltas = [0, 0.005, 0.02, 0.08];
+e_types = [e{:,1}];
+e_projections = [e_types.Projection];
+deltas = [e_projections.delta];
+
 for di=1:length(deltas)
     for v=1:3
         savefile = fullfile('data', sprintf('SIM_d%.3f_v%d', deltas(di), v), 'preprocessed.mat');
-        load(savefile);
+        disp(savefile);
+        
+        pops_task = Convert_Sampling_Output(e(di,:), 20, 20, 1000, 5);
         pops_fix = Convert_Sampling_Tuning(otune{di}, pops_task, 30);
         
         % do further preprocessing
         [pops_task, pops_fix] = Match_Corresponding_Populations(pops_task, pops_fix); % should keep everything...
         
-
+        disp('fitting');
         pops_task = Split_Conditions( pops_task );
         pops_task = Compute_fPrime_stimulus_means( pops_task );
         pops_task = Compute_fPrime_bestfit( pops_task, pops_fix );
         pops_task = Compute_fPrime_fixation_means( pops_task, pops_fix );
         
+        disp('saving');
         save(savefile, 'pops_task', 'pops_fix');
     end
 end
