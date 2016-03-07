84f9b80a8c0bfe25f5c507beeef9de579bb308a7
wrongu <rlange@ur.rochester.edu>
2016-03-07 13:48:29 -0500

    t-test significance of fprime slopes in Compute_fPrime_stimulus_means

--- Compute_fPrime_stimulus_means.m
+++ Compute_fPrime_stimulus_means.m
@@ -1,6 +1,9 @@
 function [ populations ] = Compute_fPrime_stimulus_means( populations, recompute )
 %Compute_fPrime compute f' (change in mean response with change in
 %stimulus) and return modified populations with new .fprime_stimulus_mean vector
+%
+%Also computes sensitivity; the p-value that each f' value is different
+%from zero is stored in .fprime_pvalue
 
 if nargin < 2, recompute = false; end
 
@@ -14,12 +17,17 @@ end
 
 function [pop] = Compute_fPrime_Single_Pop( pop )
 
+pop.fprime_stimulus_mean = zeros(1,length(pop.cellnos));
+pop.fprime_pvalue = zeros(1,length(pop.cellnos));
+
 for n_idx=1:length(pop.cellnos)
     % todo - Poisson variability?
     valid = ~isnan(pop.spikeRates(n_idx, :));
-    coeffs = polyfit(pop.condVec(valid), pop.spikeRates(n_idx,valid)', 1);
-    % linear fit with r(s) = coeffs(1)*s + coeffs(0)
-    pop.fprime_stimulus_mean(n_idx) = coeffs(1);
+    stats = regstats(pop.spikeRates(n_idx,valid)', pop.condVec(valid), ...
+        'linear', 'tstat');
+    % linear fit with r(s) = stats.beta(1)*s + stats.beta(2)
+    pop.fprime_stimulus_mean(n_idx) = stats.tstat.beta(1);
+    pop.fprime_pvalue = stats.tstat.pval(1);
 end
 
 end
