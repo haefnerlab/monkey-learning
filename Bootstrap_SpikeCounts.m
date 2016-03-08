7833f7fdebcea2b4902ccfe8c10c901214701c20
wrongu <rlange@ur.rochester.edu>
2016-03-08 12:20:39 -0500

    Bootstrap_SpikeCounts further bins by choices at each condition

--- Bootstrap_SpikeCounts.m
+++ Bootstrap_SpikeCounts.m
@@ -7,16 +7,19 @@ boot_pops_task = pops_task;
 for p_idx=1:length(pops_task)
     pop = pops_task(p_idx);
     
-    % separately sample spike counts for each stimulus condition
+    % separately sample spike counts for each stimulus condition + choice
+    % combination
     conditions = unique(pop.condVec);
-    for o_idx=1:length(conditions)
-        o = conditions(o_idx);
-        trials_this_condition = find(pop.condVec == o);
-        n_trials = length(trials_this_condition);
-        % resample trials at this condition (with replacement)
-        resampled_trials = randi(n_trials, 1, n_trials);
-        pop.spikeCounts(:, trials_this_condition) = ...
-            pop.spikeCounts(:, trials_this_condition(resampled_trials));
+    for s_idx=1:length(conditions)
+        s = conditions(s_idx);
+        for choice=[-1,1]
+            trials_this_condition = find(pop.condVec == s & pop.realChoice == choice);
+            n_trials = length(trials_this_condition);
+            % resample trials at this condition (with replacement)
+            resampled_trials = randi(n_trials, 1, n_trials);
+            pop.spikeCounts(:, trials_this_condition) = ...
+                pop.spikeCounts(:, trials_this_condition(resampled_trials));
+        end
     end
 end
 
