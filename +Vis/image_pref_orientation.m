64faa3cafd294bf0ac45614af1cc6bc8235fc983
wrongu <rlange@ur.rochester.edu>
2016-10-25 14:46:51 -0400

    whitespace formatting in image_pref_orientation

--- +Vis/image_pref_orientation.m
+++ +Vis/image_pref_orientation.m
@@ -1,23 +1,23 @@
 function [image, norm] = image_pref_orientation(orient_x, orient_y, xydata, varargin)
-%IMAGE_PREF_ORIENTATION 
+%IMAGE_PREF_ORIENTATION
 %
-%	I = IMAGE_PREF_ORIENTATION(orient_x, orient_y, xydata, ...) 
-%	creates a square image where the axes correspond to 0:180. Each data point 
+%	I = IMAGE_PREF_ORIENTATION(orient_x, orient_y, xydata, ...)
+%	creates a square image where the axes correspond to 0:180. Each data point
 %	xydata(i) is plotted with some width to "blend" into areas with no data.
 %   This can be done either with discs (constant contribution to all points
 %   within a radius) or with circular hills. The image is imsizeXimsize pixels.
-%   Each pixel is a weighted average from discs/hills. 
+%   Each pixel is a weighted average from discs/hills.
 %   The image is NaN where zero data are counted.
-%	
+%
 %   I = IMAGE_PREF_ORIENTATION(..., 'imsize', w) makes a w x w image
-%	
+%
 %   I = IMAGE_PREF_ORIENTATION(..., 'method', m) where m is 'disc' or
 %   'smooth' selects which method is used to fill in missing data
-%	
+%
 %   I = IMAGE_PREF_ORIENTATION(..., 'radius', r) sets the 'width' of the
 %   disc or smooth-hill kernels. It is the radius of the disc or stdev of a
 %   gaussian kernel
-%	
+%
 %   I = IMAGE_PREF_ORIENTATION(..., 'diagonal', d) a boolean flag, defaults
 %   to false. Determines whether to ignore data where orient_x == orient_y
 %
@@ -53,16 +53,16 @@ ndata = length(orient_x);
 assert(length(orient_y) == ndata);
 assert(length(xydata) == ndata);
 
-function kernel = kernel_centered_at(cx, cy)
-    % center on cx,cy and wrap around edges (toroidal)
-	shifted_xx = mod(xx - cx, 180); shifted_xx = min(shifted_xx, 180-shifted_xx);
-	shifted_yy = mod(yy - cy, 180); shifted_yy = min(shifted_yy, 180-shifted_yy);
-	kernel = exp(-0.5*(shifted_xx.^2 + shifted_yy.^2) / (radius^2));
-    if strcmp(method, 'disc')
-        % disc is just binarized gaussian at 1 std deviation
-        kernel = kernel > 0.67;
+    function kernel = kernel_centered_at(cx, cy)
+        % center on cx,cy and wrap around edges (toroidal)
+        shifted_xx = mod(xx - cx, 180); shifted_xx = min(shifted_xx, 180-shifted_xx);
+        shifted_yy = mod(yy - cy, 180); shifted_yy = min(shifted_yy, 180-shifted_yy);
+        kernel = exp(-0.5*(shifted_xx.^2 + shifted_yy.^2) / (radius^2));
+        if strcmp(method, 'disc')
+            % disc is just binarized gaussian at 1 std deviation
+            kernel = kernel > 0.67;
+        end
     end
-end
 
 for i = 1:ndata
     if diagonal || (orient_x(i) ~= orient_y(i))
